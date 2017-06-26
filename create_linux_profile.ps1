57cd72f75a583a74239ff18cc3cb7fde3e789cdb
SNaP <pc@ad.local>
2017-06-26 01:12:52 -0400

    asdf

--- create_linux_profile.ps1
+++ create_linux_profile.ps1
@@ -1,10 +1,12 @@
+mkdir -p $HOME/.local 2>&1 | Out-Null
+mkdir -p $HOME/.local/share 2>&1 | Out-Null
+mkdir -p $HOME/.local/share/powershell 2>&1 | Out-Null
+mkdir -p $HOME/.local/share/powershell/Modules 2>&1 | Out-Null
+unzip -n ./Include/PowerCLI.ViCore.zip -d $HOME/.local/share/powershell/Modules 2>&1 | Out-Null
+unzip -n ./Include/PowerCLI.Vds.zip -d $HOME/.local/share/powershell/Modules 2>&1 | Out-Null
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false 2>&1 | Out-Null
+
 "Get-Module -ListAvailable PowerCLI* | Import-Module
 . `"$HOME/WindowsPowerShell/Modules/zCore/init.ps1`"" | New-Item -Path ../.config/powershell -Name Microsoft.PowerShell_profile.ps1 -Force -Confirm:$false 2>&1 | Out-Null
-#mkdir $CD/.local > /dev/null
-#mkdir $HOME/.local/share > /dev/null
-#mkdir $HOME/.local/share/powershell > /dev/null
-#mkdir $HOME/.local/share/powershell/Modules > /dev/null
-unzip ./Include/PowerCLI.ViCore.zip -d $HOME/.local/share/powershell/Modules > /dev/null
-unzip ./Include/PowerCLI.Vds.zip -d $HOME/.local/share/powershell/Modules > /dev/null
-powershell -Command "Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:\$false"
+
 exit; exit
