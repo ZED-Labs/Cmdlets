a5bd51a4307c9008f05066f9e046dff854b4c8f8
SNaP <pc@ad.local>
2017-06-26 05:08:41 -0400

    asdf

--- create_linux_profile.ps1
+++ create_linux_profile.ps1
@@ -9,10 +9,10 @@ mkdir -p $HOME/.local/share/powershell/Modules 2>&1 | Out-Null
 if (Test-Path $profile){
     if (! (Get-Content $Profile | Where {$_ -like "*init.ps1*"})){
         $orig_profile = get-content $Profile
-        'If (Test-Path "$HOME/WindowsPowerShell/Modules/zCore/init.ps1"){. "$HOME/WindowsPowerShell/Modules/zCore/init.ps1"}'+"`n$orig_profile" | New-Item -Path ../.config/powershell -Name Microsoft.PowerShell_profile.ps1 -Force -Confirm:$false 2>&1 | Out-Null
+        'If (Test-Path "$HOME/WindowsPowerShell/Modules/zCore/init.ps1"){. "$HOME/WindowsPowerShell/Modules/zCore/init.ps1"}'+"`n$orig_profile" | New-Item -Path $HOME/.config/powershell -Name Microsoft.PowerShell_profile.ps1 -Force -Confirm:$false 2>&1 | Out-Null
     }
 }else{
-    'If (Test-Path "$HOME/WindowsPowerShell/Modules/zCore/init.ps1"){. "$HOME/WindowsPowerShell/Modules/zCore/init.ps1"}' | New-Item -Path ../.config/powershell -Name Microsoft.PowerShell_profile.ps1 -Force -Confirm:$false 2>&1 | Out-Null
+    'If (Test-Path "$HOME/WindowsPowerShell/Modules/zCore/init.ps1"){. "$HOME/WindowsPowerShell/Modules/zCore/init.ps1"}' | New-Item -Path $HOME/.config/powershell -Name Microsoft.PowerShell_profile.ps1 -Force -Confirm:$false 2>&1 | Out-Null
 }
 
 write-host "Cmdlets have been successfully install!`n" -ForegroundColor Cyan
