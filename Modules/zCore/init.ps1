e9da1ae088f959305881abcf85c730a1d69df596
SNaP <pc@ad.local>
2017-06-25 22:59:21 -0400

    Started integrating Linux

--- Modules/zCore/init.ps1
+++ Modules/zCore/init.ps1
@@ -1,9 +1,10 @@
 write-host "Cmdlet initialization has started..." -ForegroundColor Yellow -NonewLine
 if ($profile -like "*\*"){
 	$ProfilePath = $profile.Replace($profile.split("\")[-1],"").trim('\')
+    $ModulePath = "$ProfilePath\Modules"
 }
 if ($profile -like "*/*"){
 	$ProfilePath = '/'+$profile.Replace($profile.split("/")[-1],"").trim('/')
 	$ModulePath = $env:PSMODULEPATH.Split(':') | Where {$_ -like "*$ENV:HOME*"}
 }
-write-host "  my profile path:  $ProfilePath"
+write-host "`n  my profile path:  $ProfilePath`n  my module path: $ModulePath"
