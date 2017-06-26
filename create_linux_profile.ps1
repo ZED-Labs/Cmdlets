0ee8b5c6d2e6cc130dfd507ac3a57a8a6b4c4c54
SNaP <pc@ad.local>
2017-06-26 04:18:47 -0400

    asdf

--- create_linux_profile.ps1
+++ create_linux_profile.ps1
@@ -2,10 +2,10 @@ mkdir -p $HOME/.local 2>&1 | Out-Null
 mkdir -p $HOME/.local/share 2>&1 | Out-Null
 mkdir -p $HOME/.local/share/powershell 2>&1 | Out-Null
 mkdir -p $HOME/.local/share/powershell/Modules 2>&1 | Out-Null
-unzip -n ./Include/PowerCLI.ViCore.zip -d $HOME/.local/share/powershell/Modules 2>&1 | Out-Null
-unzip -n ./Include/PowerCLI.Vds.zip -d $HOME/.local/share/powershell/Modules 2>&1 | Out-Null
-Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false 2>&1 | Out-Null
+# unzip -n ./Include/PowerCLI.ViCore.zip -d $HOME/.local/share/powershell/Modules 2>&1 | Out-Null
+# unzip -n ./Include/PowerCLI.Vds.zip -d $HOME/.local/share/powershell/Modules 2>&1 | Out-Null
+# Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false 2>&1 | Out-Null
 
-". `"$HOME/WindowsPowerShell/Modules/zCore/init.ps1`"" | New-Item -Path ../.config/powershell -Name Microsoft.PowerShell_profile.ps1 -Force -Confirm:$false 2>&1 | Out-Null
+'If (Test-Path "$HOME/WindowsPowerShell/Modules/zCore/init.ps1"){. "$HOME/WindowsPowerShell/Modules/zCore/init.ps1"}' | New-Item -Path ../.config/powershell -Name Microsoft.PowerShell_profile.ps1 -Force -Confirm:$false 2>&1 | Out-Null
 
 exit; exit
