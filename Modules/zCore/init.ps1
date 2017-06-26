ac5364702df55da232d866e42b8497c32445ac3c
SNaP <pc@ad.local>
2017-06-25 22:42:44 -0400

    Profile Path

--- Modules/zCore/init.ps1
+++ Modules/zCore/init.ps1
@@ -1,1 +1,9 @@
-write-host "Cmdlet initialization has started..."
+write-host "Cmdlet initialization has started..." -ForegroundColor Yellow -NonewLine
+if ($profile -like "*\*"){
+	$ProfilePath = $profile.Replace($profile.split("\")[-1],"").trim('\')
+}
+if ($profile -like "*/*"){
+	$ProfilePath = '/'+$profile.Replace($profile.split("/")[-1],"").trim('/')
+	$ModulePath = $env:PSMODULEPATH.Split(':') | Where {$_ -like "*$ENV:HOME*"}
+}
+write-host "  my profile path:  $ProfilePath"
