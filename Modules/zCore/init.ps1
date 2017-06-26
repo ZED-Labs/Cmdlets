65359a3f121ab4cb234e651264c304319a42f235
SNaP <pc@ad.local>
2017-06-25 23:57:38 -0400

    global var

--- Modules/zCore/init.ps1
+++ Modules/zCore/init.ps1
@@ -1,12 +1,12 @@
 write-host "Initializing Cmdlets..." -ForegroundColor Yellow -NonewLine
 function zInit(){
 	if ($profile -like "*\*"){
-		$ProfilePath = $profile.Replace($profile.split("\")[-1],"").trim('\')
-		$ModulePath = "$ProfilePath\Modules"
+		$Global:ProfilePath = $profile.Replace($profile.split("\")[-1],"").trim('\')
+		$Global:ModulePath = "$ProfilePath\Modules"
 	}
 	if ($profile -like "*/*"){
-		$ProfilePath = '/'+$profile.Replace($profile.split("/")[-1],"").trim('/')
-		$ModulePath = "$HOME/WindowsPowerShell/Modules"
+		$Global:ProfilePath = '/'+$profile.Replace($profile.split("/")[-1],"").trim('/')
+		$Global:ModulePath = "$HOME/WindowsPowerShell/Modules"
 	}
 }
 
