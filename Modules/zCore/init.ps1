698e27d842e37e15e8bf72f1be67fd4feed0b8f9
SNaP <pc@ad.local>
2017-06-25 23:25:27 -0400

    init updates

--- Modules/zCore/init.ps1
+++ Modules/zCore/init.ps1
@@ -5,6 +5,6 @@ if ($profile -like "*\*"){
 }
 if ($profile -like "*/*"){
 	$ProfilePath = '/'+$profile.Replace($profile.split("/")[-1],"").trim('/')
-	$ModulePath = $env:PSMODULEPATH.Split(':') | Where {$_ -like "*$ENV:HOME*"}
+	$ModulePath = "$ProfilePath\Modules"
 }
 write-host "`n  my profile path:  $ProfilePath`n  my module path: $ModulePath"
