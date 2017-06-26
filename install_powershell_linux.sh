19e110fd95cc3b8f4d269386b4e288f847cc6f54
SNaP <pc@ad.local>
2017-06-26 00:54:32 -0400

    asdf

--- install_powershell_linux.sh
+++ install_powershell_linux.sh
@@ -7,11 +7,18 @@ echo Installing...
 echo    libicu, libunwind, unzip, and uuid...
 yum install -y libicu libunwind unzip uuid > /dev/null
 echo    openssl...
-rpm -ivh ./Include/libcurl-openssl-7.51.0-2.1.el7.cern.x86_64.rpm > /dev/null
+if ! rpm -qa | grep libcurl-openssl-7.51.0-2.1.el7; then
+    rpm -ivh ./Include/libcurl-openssl-7.51.0-2.1.el7.cern.x86_64.rpm > /dev/null
+fi
 echo export LD_LIBRARY_PATH=/opt/shibboleth/lib64/:\$LD_LIBRARY_PATH >> /etc/bashrc
 export LD_LIBRARY_PATH=/opt/shibboleth/lib64/:$LD_LIBRARY_PATH
 echo    powershell core...
-rpm -ivh ./Include/powershell-6.0.0_beta.3-1.el7.x86_64.rpm > /dev/null
+if ! rpm -qa | grep powershell-6.0.0_beta.3-1.el7; then
+    if rpm -qa | grep powershell; then
+        rpm -e powershell
+    fi
+    rpm -ivh ./Include/powershell-6.0.0_beta.3-1.el7.x86_64.rpm > /dev/null
+fi
 echo    powercli...
 mkdir -p /usr/local/share/powershell > /dev/null
 mkdir -p /usr/local/share/powershell/Modules > /dev/null
