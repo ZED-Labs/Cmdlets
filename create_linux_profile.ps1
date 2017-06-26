c0b74ea08ca2dc11b82fd26ecd3e68c134523dc6
SNaP <pc@ad.local>
2017-06-25 23:04:50 -0400

    fixed linux path

--- create_linux_profile.ps1
+++ create_linux_profile.ps1
@@ -1,2 +1,3 @@
 "Get-Module -ListAvailable PowerCLI* | Import-Module
-. /root/.local/share/powershell/Modules/zCore/init.ps1" | New-Item -Path .config/powershell -Name Microsoft.PowerShell_profile.ps1 -Force -Confirm:$false
+. /root/.local/share/powershell/Modules/zCore/init.ps1" | New-Item -Path ../.config/powershell -Name Microsoft.PowerShell_profile.ps1 -Force -Confirm:$false
+exit
