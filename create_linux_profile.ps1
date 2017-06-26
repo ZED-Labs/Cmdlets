c791b20c99455bfac99c8d2941cc2dfedc4f2bf6
SNaP <pc@ad.local>
2017-06-25 23:13:29 -0400

    test

--- create_linux_profile.ps1
+++ create_linux_profile.ps1
@@ -1,3 +1,3 @@
 "Get-Module -ListAvailable PowerCLI* | Import-Module
 . $HOME/WindowsPowerShell/Modules/zCore/init.ps1" | New-Item -Path ../.config/powershell -Name Microsoft.PowerShell_profile.ps1 -Force -Confirm:$false
-exit
+exit; exit
