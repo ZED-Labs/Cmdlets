2bce8e1133588cf44975adf846e8136f3d36b2f9
SNaP <pc@ad.local>
2017-06-26 02:22:32 -0400

    asdf

--- install_powershell_linux.sh
+++ install_powershell_linux.sh
@@ -19,3 +19,13 @@ if ! rpm -qa | grep powershell-6.0.0_beta.3-1.el7; then
     fi
     rpm -ivh ./Include/powershell-6.0.0_beta.3-1.el7.x86_64.rpm > /dev/null
 fi
+echo    powercli...
+#mkdir -p /usr/local/share/powershell > /dev/null
+#mkdir -p /usr/local/share/powershell/Modules > /dev/null
+#/usr/local/share/powershell/Modules
+
+#unzip -n ./Include/PowerCLI.ViCore.zip -d /usr/local/share/powershell/Modules > /dev/null
+#unzip ./Include/PowerCLI.ViCore.zip -d $HOME/.local/share/powershell/Modules > /dev/null
+#echo    powercli-Vds...
+# unzip -n ./Include/PowerCLI.Vds.zip -d /usr/local/share/powershell/Modules > /dev/null
+#unzip ./Include/PowerCLI.Vds.zip -d $HOME/.local/share/powershell/Modules > /dev/null
