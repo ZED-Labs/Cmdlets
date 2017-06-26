e2d74402e5207766b95b11d1689a031a91473d90
SNaP <pc@ad.local>
2017-06-26 00:43:12 -0400

    escape $false

--- install_powershell_linux.sh
+++ install_powershell_linux.sh
@@ -21,4 +21,4 @@ unzip -n ./Include/PowerCLI.ViCore.zip -d /usr/local/share/powershell/Modules > 
 echo    powercli-Vds...
 unzip -n ./Include/PowerCLI.Vds.zip -d /usr/local/share/powershell/Modules > /dev/null
 #unzip ./Include/PowerCLI.Vds.zip -d $HOME/.local/share/powershell/Modules > /dev/null
-powershell -Command "Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false"
+powershell -Command "Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:\$false"
