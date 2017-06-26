9cb7e35096f27bc34785aa7f82c90c513214a91d
SNaP <pc@ad.local>
2017-06-26 04:22:18 -0400

    asdf

--- create_linux_profile.ps1
+++ create_linux_profile.ps1
@@ -8,5 +8,4 @@ mkdir -p $HOME/.local/share/powershell/Modules 2>&1 | Out-Null
 
 'If (Test-Path "$HOME/WindowsPowerShell/Modules/zCore/init.ps1"){. "$HOME/WindowsPowerShell/Modules/zCore/init.ps1"}' | New-Item -Path ../.config/powershell -Name Microsoft.PowerShell_profile.ps1 -Force -Confirm:$false 2>&1 | Out-Null
 
-write-host "Cmdlets have been successfully install!`n"
-Mods
+write-host "Cmdlets have been successfully install!" -ForegroundColor Cyan
