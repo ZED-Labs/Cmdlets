08524f5e55fc417ce98ee9bb3f4448920488fafc
SNaP <pc@ad.local>
2017-06-26 04:46:51 -0400

    asdf

--- create_linux_profile.ps1
+++ create_linux_profile.ps1
@@ -9,7 +9,7 @@ mkdir -p $HOME/.local/share/powershell/Modules 2>&1 | Out-Null
 if (Test-Path $profile){
     if (! (Get-Content $Profile | Where {$_ -like "*init.ps1*"})){
         $orig_profile = get-content $Profile
-        "$orig_profile"+'If (Test-Path "$HOME/WindowsPowerShell/Modules/zCore/init.ps1"){. "$HOME/WindowsPowerShell/Modules/zCore/init.ps1"}' | New-Item -Path ../.config/powershell -Name Microsoft.PowerShell_profile.ps1 -Force -Confirm:$false 2>&1 | Out-Null
+        "$orig_profile`n"+'If (Test-Path "$HOME/WindowsPowerShell/Modules/zCore/init.ps1"){. "$HOME/WindowsPowerShell/Modules/zCore/init.ps1"}' | New-Item -Path ../.config/powershell -Name Microsoft.PowerShell_profile.ps1 -Force -Confirm:$false 2>&1 | Out-Null
     }
 }else{
     'If (Test-Path "$HOME/WindowsPowerShell/Modules/zCore/init.ps1"){. "$HOME/WindowsPowerShell/Modules/zCore/init.ps1"}' | New-Item -Path ../.config/powershell -Name Microsoft.PowerShell_profile.ps1 -Force -Confirm:$false 2>&1 | Out-Null
