0c0d1ed4bddc549533a107ccc478fc62529dc135
SNaP <pc@ad.local>
2017-06-26 00:40:59 -0400

    install-linux

--- install_powershell_linux.sh
+++ install_powershell_linux.sh
@@ -9,16 +9,16 @@ export LD_LIBRARY_PATH=/opt/shibboleth/lib64/:$LD_LIBRARY_PATH
 echo    powershell core...
 rpm -ivh ./Include/powershell-6.0.0_beta.3-1.el7.x86_64.rpm > /dev/null
 echo    powercli...
-mkdir /usr/local/share/powershell > /dev/null
-mkdir /usr/local/share/powershell/Modules > /dev/null
+mkdir -p /usr/local/share/powershell > /dev/null
+mkdir -p /usr/local/share/powershell/Modules > /dev/null
 #mkdir $CD/.local > /dev/null
 #mkdir $HOME/.local/share > /dev/null
 #mkdir $HOME/.local/share/powershell > /dev/null
 #mkdir $HOME/.local/share/powershell/Modules > /dev/null
 
-unzip ./Include/PowerCLI.ViCore.zip -d /usr/local/share/powershell/Modules > /dev/null
+unzip -n ./Include/PowerCLI.ViCore.zip -d /usr/local/share/powershell/Modules > /dev/null
 #unzip ./Include/PowerCLI.ViCore.zip -d $HOME/.local/share/powershell/Modules > /dev/null
 echo    powercli-Vds...
-unzip ./Include/PowerCLI.Vds.zip -d /usr/local/share/powershell/Modules > /dev/null
+unzip -n ./Include/PowerCLI.Vds.zip -d /usr/local/share/powershell/Modules > /dev/null
 #unzip ./Include/PowerCLI.Vds.zip -d $HOME/.local/share/powershell/Modules > /dev/null
-Powershell -Command "Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false"
+powershell -Command "Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false"
